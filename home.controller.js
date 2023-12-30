3ae774ab19e189c9480f1a807ee629bdd6932265
Thuong <dangthuong1067@gmail.com>
2023-12-30 16:32:23 +0700

    sửa lại logic

--- home.controller.js
+++ home.controller.js
@@ -3,27 +3,16 @@ const { products, banners, categories } = require('./data');
 
 exports.getProducts = (req, res) => {
   const { tag, categoryId } = req.query;
-  let saleProducts = [];
-  let popularProducts = [];
-  let productByCategory = [];
+  let filterProducts = []
 
   if (tag) {
-    products.forEach(item => {
-      let tags = item.tags;
-      if (tags?.length > 0) {
-        tags.forEach(element => {
-          if (tag === 'sale' && element === tag) {
-            saleProducts.push(item);
-          } else if (element === 'popular' && element === tag) {
-            popularProducts.push(item);
-          }
-        });
-      }
-    });
+    filterProducts = products.filter(item => {
+      return item?.tags?.includes(tag);
+    })
   }
 
   if (categoryId) {
-    productByCategory = products.filter(item => item.categoryId === Number(categoryId));
+    filterProducts = products.filter(item => item.categoryId === Number(categoryId));
   }
 
   res
@@ -31,8 +20,7 @@ exports.getProducts = (req, res) => {
     .json({
       status: 'success',
       data: {
-        products: tag === 'sale' ? saleProducts : tag === 'popular' ? popularProducts : categoryId ? productByCategory : products,
-        productByCategory
+        products: filterProducts,
       }
     });
 };
